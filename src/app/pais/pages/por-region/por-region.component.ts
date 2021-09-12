d9698e0b5b6a5e70eeecbbb63505e44899712613
Angie Jimenez <angiejimenez@gmail.com>
2021-09-12 12:14:11 +0200

    region upgrade

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -6,36 +6,36 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [ `
-    button {
-    margin-right: 5px;
-  }
-  `
-  ]
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
 export class PorRegionComponent {
+  regiones: string[] = ['africa', 'america', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-regiones: string[]= ['africa', 'americas', 'asia', 'europe', 'oceania'];
-regionActiva: string = '';
-paises: Country[]=[];
+  constructor(private paisService: PaisService) {}
 
-  constructor(private paisService:PaisService) { }
-
-  getClassCSS(region: string){
-    return (region === this.regionActiva)
-             ? 'btn btn-primary'
-             : 'btn btn-outline-primary';
+  getClassCSS(region: string) {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
-  activarRegion(region:string){
-
-if (region === this.regionActiva){return};
-   this.regionActiva = region;
-   this.paises= [];
-   this.paisService.buscarRegion(region)
-   .subscribe( paises=> this.paises = paises);
-//TODO: Hacer el llamado al servicio
-
- }
-
+  activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService
+      .buscarRegion(region)
+      .subscribe((paises) => (this.paises = paises));
+    //TODO: Hacer el llamado al servicio
+  }
 }
